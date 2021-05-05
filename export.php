31247cae2d1c7465120e79aefed2e17497b72cc6
Shunsuke Kimura <me@kimurashunsuke.com>
2021-05-06 00:13:16 +0900

    seleniumIDEのスクリプトファイルを自動生成

--- export.php
+++ export.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
 define('BASE_DIR', '/var/www/html');
 set_include_path(get_include_path() . PATH_SEPARATOR . BASE_DIR);
 
@@ -7,3 +8,10 @@ require_once 'UploadDropbox.php';
 
 DrawPng::handle();
 UploadDropbox::handle(BASE_DIR . '/export.png');
+
+$script = file_get_contents(BASE_DIR . '/suzuri.side.base');
+$script = str_replace('${date}', date('Y/m/d'), $script);
+$fh = fopen(BASE_DIR . '/suzuri.side', 'w');
+fwrite($fh, $script);
+fclose($fh);
+UploadDropbox::handle(BASE_DIR . '/suzuri.side');
